94f46c91c286647ed308df27f5d83be41bf26ea1
Yatouwu <154877343+Yatouwu@users.noreply.github.com>
2024-11-06 01:24:31 +0800

    install a vitest

--- svelte.config.js
+++ svelte.config.js
@@ -12,6 +12,11 @@ const config = {
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter({ pages: "build", assets: "build", fallback: undefined, precompress: false, strict: true }),
+
+		paths: {
+			base: process.env.NODE_ENV === 'production' ? '/project2' : ''
+		  },
+		  
 		prerender: {
 			handleHttpError: ({ path, status }) => {
 				// Log or handle 404 errors gracefully during the build
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,11 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { defineConfig } from "vitest/config";
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()], // Add a comma here to separate properties
+
+	// Configure Vitest if you're using it for testing
+	test: {
+		include: ['src/**/*.{test,spec}.{js,ts}']
+	}
 });
