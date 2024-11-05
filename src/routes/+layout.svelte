a0bd3fe8dd4e10b7cdae9a5a3b4fad96a29fab73
Yatouwu <154877343+Yatouwu@users.noreply.github.com>
2024-11-06 02:37:48 +0800

    adding home page and some style

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,1 +1,23 @@
-<script>import "../app.css";</script><slot></slot>
+<script>
+    import "../app.css";
+  
+    let navItems = [
+      { name: "Home", href: "/home" },
+      { name: "Events", href: "/events" },
+      { name: "Contacts", href: "/contacts" },
+    ];
+  </script>
+  
+  <div class="navbar">
+    <h1 class="brand">EventNex</h1>
+    <nav>
+      {#each navItems as item}
+        <a href={item.href} class="nav-link">{item.name}</a>
+      {/each}
+    </nav>
+  </div>
+  
+  <main>
+    <slot />
+  </main>
+  
---
+++ src/routes/home/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+    import './home.css';
+</script>
+
+<h1 class="tagline">One Stop</h1>
+<h2 class="title">Event Planner</h2>
+<p class="subtitle">EVERY EVENT SHOULD BE MANAGE</p>
